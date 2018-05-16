a5771682bdef57246e27e253a842d4e98bb6332b
Colm Hughes <cjhugo1991@hotmail.com>
2018-05-16 16:15:18 +0000

    Updated

--- contacts.py
+++ contacts.py
@@ -1,37 +1,37 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 from flask import render_template
 
 app = Flask(__name__)
 
 
 people = {
-    'John'   : {'name': 'John',   'email': 'Johnjoe@gmail.com',    'phone': '0861234567'},    
-    'Olivia' : {'name': 'Olivia', 'email': 'Livtexting.com',       'phone': '0891122334'},
-    'Shane'  : {'name': 'Shane',  'email': 'unoriginal@gmail.com', 'phone': '1123581321'},
-    'Frank'  : {'name': 'Frank',  'email': 'sinatra@hotmail.com',  'phone': '248163264128'}
+    1  : {'id': 1, 'name': 'John',   'email': 'Johnjoe@gmail.com',    'phone': '0861234567'},    
+    2  : {'id': 2, 'name': 'Olivia', 'email': 'Livtexting.com',       'phone': '0891122334'},
+    3  : {'id': 3, 'name': 'Shane',  'email': 'unoriginal@gmail.com', 'phone': '1123581321'},
+    
     }
-
+next_id = 4
 
 
 @app.route("/", methods=['GET', 'POST'])
 def add_contact():
+    global next_id
     if request.method == "POST":
         
         name = request.form.get('name')
         email = request.form.get('email')
         phone = request.form.get('phone')
-        # people.append({'name': name, 'email': email, 'phone': phone})
-        people[name] = {'name': name, 'email': email, 'phone': phone}
-    
+        id = next_id
+        people[id] = {'id': id, 'name': name, 'email': email, 'phone': phone}
+        next_id += 1
     return render_template("contact.html", data=people.values())
 
 @app.route("/delete", methods=['POST'])
 def delete_contact():
-    name_to_delete = request.form.get('contact_to_delete')
+    id_to_delete = int(request.form.get('contact_to_delete'))
     
-    del(people[name_to_delete])
-            
-    return render_template("contact.html", data=people.values())
+    del(people[id_to_delete])
+    return redirect("/")       
 
 
 if __name__ == "__main__":
